807bcf8f9373f8bc459f4e60b01335c8b3e57e86
jaop2316 <jessikita.ldu@gmail.com>
2017-10-24 10:41:24 -0500

    Add js compiled files for netcodec theme

---
+++ themes/custom/netcodec/_src/js/main.js
@@ -0,0 +1,13 @@
+(function ($) {
+
+  Drupal.behaviors.main = {
+    attach: function (context, settings) {
+      "use strict";
+
+      console.log("It works, add js");
+
+    }
+  };
+
+
+})(jQuery);
---
+++ themes/custom/netcodec/js/script.js
@@ -0,0 +1,14 @@
+(function ($) {
+
+  Drupal.behaviors.main = {
+    attach: function (context, settings) {
+      "use strict";
+
+      console.log("It works, add js");
+
+    }
+  };
+
+
+})(jQuery);
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm1haW4uanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoic2NyaXB0LmpzIiwic291cmNlc0NvbnRlbnQiOlsiKGZ1bmN0aW9uICgkKSB7XG5cbiAgRHJ1cGFsLmJlaGF2aW9ycy5tYWluID0ge1xuICAgIGF0dGFjaDogZnVuY3Rpb24gKGNvbnRleHQsIHNldHRpbmdzKSB7XG4gICAgICBcInVzZSBzdHJpY3RcIjtcblxuICAgICAgY29uc29sZS5sb2coXCJJdCB3b3JrcywgYWRkIGpzXCIpO1xuXG4gICAgfVxuICB9O1xuXG5cbn0pKGpRdWVyeSk7Il19
